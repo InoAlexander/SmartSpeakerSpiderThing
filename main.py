1a53a02d1246eafb58f06015a87f67e6f8aaa49d
Ino_A <inoalexander007@gmail.com>
2022-01-09 19:17:02 -0800

    working on taking commands and responding

--- main.py
+++ main.py
@@ -12,19 +12,30 @@ voices = engine.getProperty('voices')
 
 # telling the engine to set the paroperty in accordance to the library used, reference the docs
 # engine.setProperty('voice', voices[anyInt].id)
-
-#this is what frank will say when activated intially by his name
 engine.say('i am the friendly robotic automata, now conceptualized, call me franc for short, my creator is eeno alexander, i am awaiting command')
-engine.runAndWait()
 
-try:
-    with sr.Microphone() as source:
-        print('listening...')
-        voice = listener.listen(source) # this is listening for  a source which would be the microphone listening for voice input
-        command = listener.recognize_google(voice) # uses google voice 
-        command = command.lower()#computers like lowercase letters and it makes it easier for it to interpret what is being said.
-        if 'frank' in command:
+def speak(text):
+    #this is what frank will say when activated intially by his name
+    engine.say(text)
+    engine.runAndWait()
 
-            print(command)
-except:
-    pass
+def prompt_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source) # this is listening for  a source which would be the microphone listening for voice input
+            command = listener.recognize_google(voice) # uses google voice 
+            command = command.lower()#computers like lowercase letters and it makes it easier for it to interpret what is being said.
+            if 'frank' in command:
+                command = command.replace
+                print(command)
+    except:
+        pass
+    return command
+
+# will take the command function from user from the prompt_command function above.
+def user_command():
+    command = prompt_command()
+    if 'play' in command:
+        
+        print('playing')
